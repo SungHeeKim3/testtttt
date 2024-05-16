de9b7af60722538c10231fbc3d6174e355e5b6f4
sunghee.kim <sunghee3.kim@gmail.com>
2024-05-16 16:14:00 +0900

    fdgsdfg

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-+ACM-include +ADw-iostream+AD4-
-using namespace std+ADs-
+#include <iostream>
+using namespace std;
 
 int main()
-+AHs-
-    cout +ADwAPA- +ACI- WTF+ACI- +ADwAPA- endl+ADs-
-    cout +ADwAPA- +ACI-asdfasdf+ACI- +ADwAPA- endl+ADs- //+u1DFfA-
-+AH0-
+{
+    cout << " WTF" << endl;
+    cout << "asdfasdf" << endl; //뭐야
+}
